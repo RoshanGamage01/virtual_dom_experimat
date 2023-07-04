69a433c57d55900f02f86d310da0bfcee97d4c70
Roshan Gamage <apple.roshangamage@gmail.com>
2023-07-04 21:06:14 +0530

    test

--- Tag.js
+++ Tag.js
@@ -4,27 +4,47 @@ class Tag{
         this.attr = attr;
         this.html = html;
         this.text = text;
+        this.title;
     }
 
     genTag(){
-        let tagName = this.tagName;
-        let attr = this.attr;
-        let text = this.text;
         let html = "";
 
         if(this.html !== []){
             this.html.forEach(e => {
-                const el = new Tag(e.name, e.attribute, e.html, e.text);
-                html = el.genTag();
+                const el = new Tag(e.name, e.attributes, e.html, e.text);
+                html += el.genTag()+"\n";
             });
         }
 
-        return `
-            <${this.tagName}>
-                ${html === "" ? "" : html}
-                ${this.text}
-            </${this.tagName}>
-        `
+        return `<${this.tagName} ${this.parseAttributes()}>${html === "" ? "" : html}${this.text}</${this.tagName}>`;
+    }
+
+    parseAttributes(){
+        let attributes = "";
+
+        if(this.attr !== []){
+            // console.log(this.attr);
+            this.attr.forEach(att => {
+                attributes += `${att.name} = "${att.value}"`
+            })
+        }
+
+        return attributes;
+    }
+
+    genPage(){
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>${this.title !== "" ? this.title : "Document"}</title>
+        </head>
+        <body>
+            ${ this.genTag() }
+        </body>
+        </html>`
     }
 }
 
--- transpiler.js
+++ transpiler.js
@@ -7,11 +7,13 @@ const numOfPages = data.length;
 for(let i = 0; i < numOfPages; i++){
     let content = "";
     data[i].elements.forEach(element => {
+        console.log(element)
         const e = new Tag(element.name, element.attributes, element.html, element.text);
-        content = e.genTag();
+        e.title = data[i].head.title;
+        content += e.genTag();
     })
 
-    fs.writeFile('index.html', content, function (err) {
+    fs.writeFile(`${data[i].documentName}.html`, content, function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
