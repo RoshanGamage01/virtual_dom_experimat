3407a383cb98e35427203b759fd0c1627c070f52
Roshan Gamage <apple.roshangamage@gmail.com>
2023-07-07 16:16:22 +0530

    update

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-const obj = document.createElement("div")
+// const obj = document.createElement("div")
 
-let handler = {
-    get: function(obj, prop){
-        return obj
-    }
-}
+// let handler = {
+//     get: function(obj, prop){
+//         return obj
+//     }
+// }
 
-console.log(new Proxy(obj, handler))
+// console.log(new Proxy(obj, handler))
 
 
-function createNode(nodeName){
-    return document.createElement(nodeName);
-}
-
-function addAttributes(node, attributes){
-    const el = node;
-
-    new Proxy(node, {
-        set: function(obj, prop, value){
-
-        }
-    })
+function createNode(nodeName, attributes, child){
+    if(typeof nodeName)
 }
